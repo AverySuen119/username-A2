87c425fc1c9b543ef7f6dfa05ceb76fab0b68607
AverySuen119 <r.sun.17@student.scu.edu.au>
2024-12-03 10:29:09 +0800

    Create AssignmentTwo.java

---
+++ src/AssignmentTwo.java
@@ -0,0 +1,6 @@
+public class AssignmentTwo {
+    public static void main(String[] args) {
+        // This is where the code to run your program will go
+        System.out.println("Hello, Assignment Two!");
+    }
+}
